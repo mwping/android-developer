ffa3014641f0a1a8aa0389d8217804bc38ed55c9
孟卫平 <mwping1324@163.com>
2021-09-10 17:43:47 +0800

    feat:  增加 Flow zip sample

---
+++ Android-developer-samples/app/src/main/java/com/mwping/android/developer/samples/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mwping.android.developer.samples
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
